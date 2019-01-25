63b5338ad178a47c21ca232d4043558764b7b31f
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-25 16:01:20 +0000

    tests for root url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # for heroku testing purposes
-  root 'application#hello'
+  # root 'application#hello'
+  root 'static_pages#home'
   get 'static_pages/home'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,7 +5,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = '| Income Markets'
   end
 
-  test "should get home" do
+  test 'should get root' do
+    get root_url
+    assert_response :success
+  end
+
+  test 'should get home' do
     get static_pages_home_url
     assert_response :success
     assert_select 'title', @base_title
