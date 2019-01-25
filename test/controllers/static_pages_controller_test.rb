d69205731ba7cafb973400961eefc49b14142c6b
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-25 15:55:57 +0000

    adding tests for tittle and refactoring application html

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = 'Income Markets'
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select 'title', @base_title
   end
-
 end
